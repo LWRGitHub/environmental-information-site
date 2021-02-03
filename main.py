f7c68f9e978b9dc4ed4f8778ddd7349f709bd7c4
benchan777 <benchan777@gmail.com>
2021-02-03 15:32:55 -0800

    added preliminary functioning yelp api features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from yelpapi import YelpAPI
 import os
 import requests
 
@@ -15,9 +16,10 @@ load_dotenv()
 mongodb_username = os.getenv('mongodb_username')
 mongodb_password = os.getenv('mongodb_password')
 mongodb_name = 'kickstarter'
-yelp_api_key = os.getenv('yelp_api_key')
+yelp_api = YelpAPI(os.getenv('yelp_api_key'), timeout_s = 3.0)
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 client = MongoClient(f"mongodb+srv://{mongodb_username}:{mongodb_password}@cluster0.uzxh5.mongodb.net/{mongodb_name}?retryWrites=true&w=majority")
 db = client[mongodb_name]
@@ -54,7 +56,6 @@ def search_store():
             lat = js['lat']
             lng = js['lon']
 
-
         else:
             ip = request.environ['HTTP_X_FORWARDED_FOR']
             response = requests.get(f"http://ip-api.com/json/{ip}")
@@ -63,8 +64,15 @@ def search_store():
             city = js['city']
             lat = js['lat']
             lng = js['lon']
+    
+        store_name = request.form.get('store_name')
+        search_results = yelp_api.search_query(term = store_name, latitude = lat, longitude = lng, categories = "vegan")
+        business_info = search_results['businesses']
 
-    return render_template('search_store.html')
+        return render_template('display_store.html', business_info = business_info)
+
+    else:
+        return render_template('search_store.html')
 
 
 
