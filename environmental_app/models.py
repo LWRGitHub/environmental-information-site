5a2a45973fa208b89c3ad64238ad0662d6c76121
benchan777 <benchan777@gmail.com>
2021-02-12 15:25:50 -0800

    fixed db name (db->db_sql)

--- environmental_app/models.py
+++ environmental_app/models.py
@@ -1,7 +1,7 @@
 from environmental_app import db_sql
 
-class User(db.Model):
+class User(db_sql.Model):
     """ User information model """
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), nullable = False, unique = True)
-    password = db.Column(db.String(80), nullable = False)
+    id = db_sql.Column(db_sql.Integer, primary_key = True)
+    username = db_sql.Column(db_sql.String(80), nullable = False, unique = True)
+    password = db_sql.Column(db_sql.String(80), nullable = False)
