fd72f03e6e9aa026cb472b713340e507951cfed5
benchan777 <benchan777@gmail.com>
2021-02-12 03:03:16 -0800

    added app initializations

--- environmental_app/__init__.py
+++ environmental_app/__init__.py
@@ -13,9 +13,26 @@ import jinja2
 from pprint import PrettyPrinter
 from random import randint
 
+mongodb_username = os.getenv('mongodb_username')
+mongodb_password = os.getenv('mongodb_password')
+mongodb_name = 'kickstarter'
+yelp_api = YelpAPI(os.getenv('yelp_api_key'), timeout_s = 3.0)
+google_maps_api_key = os.getenv('google_maps_api_key')
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.from_object(Config)
 
-client = MongoClient(f"mongodb+srv://{Config.mongodb_username}:{Config.mongodb_username}@cluster0.uzxh5.mongodb.net/{Config.mongodb_name}?retryWrites=true&w=majority")
+# Initialize flask Google Maps
+GoogleMaps(app, key = google_maps_api_key)
+
+# Initialize MongoDB
+client = MongoClient(f"mongodb+srv://{mongodb_username}:{mongodb_password}@cluster0.uzxh5.mongodb.net/{mongodb_name}?retryWrites=true&w=majority")
 db = client[mongodb_name]
+
+# Blueprints
+from environmental_app.routes import main
+app.register_blueprint(main)
+
+# with app.app_context():
+#     db.create_all()
