ab9d9d5563032fee58a0b1aea342c415543bcb5b
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-02 09:49:13 -0800

    added images for final present

--- environmental_app/config.py
+++ environmental_app/config.py
@@ -4,5 +4,7 @@ import os
 load_dotenv()
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("sqlalchemy_database_uri")
+    # SQLALCHEMY_DATABASE_URI = os.getenv("sqlalchemy_database_uri")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost/database1"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
